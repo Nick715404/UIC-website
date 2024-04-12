e7480ee01e94d519b942d39a09417c149f94cb00
nicklllson <nikita.vasia2015@gmail.com>
2024-04-12 10:37:21 +0500

    Update: Preparation for starting project

---
+++ templates/components/TemplateName.tsx
@@ -0,0 +1,9 @@
+import styles from './TemplateName.module.scss';
+
+interface Props { }
+
+export const TemplateName = ({ }: Props) => {
+  return (
+    <div className={styles.templateName}>TemplateName</div>
+  )
+}
